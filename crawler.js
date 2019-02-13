3b544ba2525fc011d8e8f5b92c48804287deba3f
apple <apple@apples-MacBook-Pro-2.local>
2019-02-13 14:09:57 +0530

    final commit

--- crawler.js
+++ crawler.js
@@ -15,21 +15,22 @@ const getAllLinksInURL = async (url) => {
             const {
                 document
             } = new JSDOM(response.data).window;
-            let aLinks = document.getElementsByTagName('a');
-            let iLinks = document.getElementsByTagName('img');
-            let allLinks =[...aLinks,...iLinks]
-            return allLinks; 
+            let aLinks = await document.getElementsByTagName('a');
+            let iLinks = await document.getElementsByTagName('img');
+            let allLinks = [...aLinks, ...iLinks]
+            return allLinks;
         } else {
             console.log(url);
             return;
         }
     } catch (error) {
         console.log(url);
+        return;
     }
 };
 
- const getCleanURL = (url) => {
-    if(url){
+const getCleanURL = (url) => {
+    if (url) {
         url = url.split("#")[0];
         url = url.split("?")[0];
         return url;
@@ -37,27 +38,27 @@ const getAllLinksInURL = async (url) => {
 };
 
 
-(async () => {
+const main = async () => {
     let allAcherPoints = await getAllLinksInURL(url) || [];
     let finalURL = [];
-    let imgLinks =[];
+    let imgLinks = [];
     let otherDomain = [];
     for (const i of allAcherPoints) {
-        if (i.href ){
+        if (i.href) {
             let cleanURL = getCleanURL(i.href);
             if (cleanURL && REGEX.test(cleanURL)) {
                 if (!finalURL.includes(cleanURL)) {
                     finalURL.push(cleanURL);
                 }
-            }else if(cleanURL && !REGEX.test(cleanURL) && !otherDomain.includes(cleanURL)){
+            } else if (cleanURL && !REGEX.test(cleanURL) && !otherDomain.includes(cleanURL)) {
                 console.log("otherlink")
                 otherDomain.push(cleanURL);
-            } 
-        }else if(i.src ){
+            }
+        } else if (i.src) {
             let cleanURL = i.src;
-            if (cleanURL ) {
-               
-                if (!finalURL.includes(cleanURL) && !imgLinks.includes(cleanURL) ) {
+            if (cleanURL) {
+
+                if (!finalURL.includes(cleanURL) && !imgLinks.includes(cleanURL)) {
                     finalURL.push(cleanURL);
                     imgLinks.push(cleanURL);
                 }
@@ -67,33 +68,33 @@ const getAllLinksInURL = async (url) => {
     for (const i of finalURL) {
         let allLinksInURL = await getAllLinksInURL(i) || [];
         for (const j of allLinksInURL) {
-            if (j.href){
+            if (j.href) {
                 let cleanURL = getCleanURL(j.href);
                 if (cleanURL && REGEX.test(cleanURL)) {
                     if (!finalURL.includes(cleanURL)) {
                         finalURL.push(cleanURL);
                     }
-                }else if(cleanURL && !REGEX.test(cleanURL) && !otherDomain.includes(cleanURL)){
+                } else if (cleanURL && !REGEX.test(cleanURL) && !otherDomain.includes(cleanURL)) {
                     otherDomain.push(cleanURL);
-                } 
-            }else if(j.src){
+                }
+            } else if (j.src) {
                 let cleanURL = j.src;
                 console.log(cleanURL);
                 if (cleanURL) {
-                    
-                    if (!finalURL.includes(cleanURL) && !imgLinks.includes(cleanURL) ) {
+
+                    if (!finalURL.includes(cleanURL) && !imgLinks.includes(cleanURL)) {
                         finalURL.push(cleanURL);
                         imgLinks.push(cleanURL);
                     }
                 }
             }
         }
-        console.log(finalURL.length," final");
-        console.log(otherDomain.length," other")
+        console.log(finalURL.length, " final");
+        console.log(otherDomain.length, " other")
     }
     console.log(imgLinks);
 
-    
+
     // for TXT Sitemap
     for (const i of finalURL) {
         fs.appendFileSync("./sitemap.txt", i + "\n")
@@ -105,7 +106,7 @@ const getAllLinksInURL = async (url) => {
     // for XML Sitemap
     fs.writeFileSync("./sitemap.xml", `<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`)
     for (const i of finalURL) {
-        if(REGEX.test(i)){
+        if (REGEX.test(i)) {
             fs.appendFileSync("./sitemap.xml", `
             <url>
                 <loc>${i}</loc>
@@ -113,10 +114,10 @@ const getAllLinksInURL = async (url) => {
                 <changefreq>always</changefreq>
                 <priority>1.0</priority>
             </url>
-                    `)  
+                    `)
 
-        }else{
-        fs.appendFileSync("./sitemap.xml", `
+        } else {
+            fs.appendFileSync("./sitemap.xml", `
 <url>
     <image>        
         <loc>${i}</loc>
@@ -126,24 +127,29 @@ const getAllLinksInURL = async (url) => {
     </image>
 </url>
         `)
-    }
+        }
     }
     for (const i of otherDomain) {
-            fs.appendFileSync("./sitemap.xml", `
+        fs.appendFileSync("./sitemap.xml", `
             <url>
                 <loc>${i}</loc>
                 <lastmod>${new Date()}</lastmod>
                 <changefreq>always</changefreq>
                 <priority>1.0</priority>
             </url>
-                    `)  
+                    `)
 
-        }
+    }
     fs.appendFileSync("./sitemap.xml", `</urlset>`)
-})()
+}
 
 
-module.exports={
-    getAllLinksInURL:getAllLinksInURL,
-    getCleanURL:getCleanURL
-}   
+
+if (process.env.NODE_ENV !== "TEST") {
+    main();
+}
+
+module.exports = {
+    getAllLinksInURL: getAllLinksInURL,
+    getCleanURL: getCleanURL
+}
--- test/test.js
+++ test/test.js
@@ -1,42 +1,44 @@
-const mocha= require('mocha');
+const mocha = require('mocha');
 const chai = require('chai');
-const expect= chai.expect;
+const expect = chai.expect;
 
 const chaiHttp = require('chai-http');
-const request= require('request');
+const request = require('request');
 
-const {getAllLinksInURL,getCleanURL} =require('../crawler')
+const {
+    getAllLinksInURL,
+    getCleanURL
+} = require('../crawler')
 const REGEX = new RegExp(/\/\/wiprodigital./, 'i');
-chai.use(chaiHttp);
+
 
 const url = "https://wiprodigital.com/";
 
-describe('Return an array of links',function(){
-    it('Returns all the links from a given url', function(done) {
-        getAllLinksInURL(url)
-        .then(res=>{
-            expect(typeof res).to.be.equals("array");
-        })
-       done();
-    });
-});
-
-
-
-describe('Return a clean url removing anything following a # or ?',function(){
-    it('Returns clean url', function(done) {
-        getAllLinksInURL(url+"#option1")
-        .then(res=>{
-            expect(res).to.be.equals("https://wiprodigital.com/");
-        })
-       done();
+describe("CRAWLER TESTING", () => {
+    describe('Return an array of links', () => {
+        it('Returns all the links from a given url', (done) => {
+            getAllLinksInURL(url).then(data => {
+                expect(typeof data).to.be.equals("object");
+                done();
+            });
+        });
     });
 
-    it('Returns clean url', function(done) {
-        getAllLinksInURL(url+"?option2")
-        .then(res=>{
-            expect(res).to.be.equals("https://wiprodigital.com/");
-        })
-       done();
+
+
+    describe('Return a clean url removing anything following a # or ?', function () {
+        it('Returns clean url', function (done) {
+            let site = url + "#option1";
+            let result = getCleanURL(site)
+            expect(result).to.be.equals("https://wiprodigital.com/");
+            done();
+        });
+
+        it('Returns clean url', function (done) {
+            let site = url + "?option2";
+            let result = getCleanURL(site)
+            expect(result).to.be.equals("https://wiprodigital.com/");
+            done();
+        });
     });
-});
+})
